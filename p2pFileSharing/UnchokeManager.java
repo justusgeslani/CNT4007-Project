29afab46e4fd99edac798ac8f6f9cc6ce9d43933
officialkzhang <kevin.zhang@ufl.edu>
2023-12-02 10:46:57 -0500

    add UnchokeManager

---
+++ p2pFileSharing/UnchokeManager.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+
+public class UnchokeManager implements Runnable {
+    private PeerProcess process;
+    private int unchokingInterval;
+    private int numberOfPreferredNeighbors;
+    private Random rnd = new Random();
+    private ScheduledFuture<?> task = null;
+    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
+    UnchokeManager(PeerProcess process) {
+        this.process = process;
+        // unchokingInterval
+        // numberOfPreferredNeighbors
+    }
+
+    public void run() {
+
+    }
+}
